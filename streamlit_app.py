5df561fa9fed4eb5acb109d08bc2f2c0d862399e
rodrigo-bernardo <rodrigo.bernardo@ua.pt>
2023-01-18 16:44:14 +0000

    18-01-23 16:44

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,42 @@
+from sqlalchemy import create_engine, text
 import streamlit as st
-import mysql.connector
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
+import time
 
-try:
-    # Connect to database
-    cnx = mysql.connector.connect(
-        host = "sql7.freesqldatabase.com",
-        user = "sql7589569",
-        password = "NTLDAUBVuH",
-        database = "sql7589569"
-    )
-    # Create a function to plot the data
-    def plot_data():
-        try:
-            # Get data from the database
-            df = pd.read_sql("SELECT * FROM molde1", cnx)
-            # Plot the data
-            plt.clf() # clear the previous plot
-            plt.plot(df['x'], df['y'])
-            plt.xlabel('X')
-            plt.ylabel('Y')
-            st.pyplot()
-        except mysql.connector.Error as error:
-            st.error(f"Error: {error}")
-    # Create a Streamlit app
-    st.title('Real-time Data Plot')
-    st.markdown("Data will auto-refresh every 5 seconds.")
-    # Add auto-refresh
-    while True:
-        plot_data()
-        st.sleep(5)
-finally:
-    if cnx.is_connected():
-        cnx.close()
-        st.success("Connection closed.")
+
+conn = create_engine("mysql://sql7589569:NTLDAUBVuH@sql7.freesqldatabase.com/sql7589569?charset=utf8mb4")
+
+st.set_page_config(layout="wide")
+
+st.write("My First App")
+
+placeholder = st.empty()
+
+if st.button("RESET"):
+    sql = text('DELETE FROM molde1;')
+    result = conn.execute(sql)
+    sql = text('ALTER TABLE molde1 AUTO_INCREMENT = 1;')
+    result = conn.execute(sql)
+    
+
+while True:
+    SQL_Query = pd.read_sql('SELECT DATAHORA,T1,P1 FROM molde1', conn)
+    df = pd.DataFrame(SQL_Query, columns=['DATAHORA','T1','P1'])
+    data = df[["T1", "P1"]]
+
+    fig = px.line(df, x="DATAHORA", y=data.columns,
+    labels={
+        "DATAHORA": "Data e Hora",
+        "value" : "Valor",
+        "variable" : "Legenda"
+    },
+    title='P1 and T1 sensor values')
+
+    fig.update_xaxes(showgrid=True, ticks="inside")
+    fig.update_layout({"uirevision": "foo"}, overwrite=True)
+
+    with placeholder.container():
+        st.plotly_chart(fig, use_container_width=True)
+
+    time.sleep(1)
