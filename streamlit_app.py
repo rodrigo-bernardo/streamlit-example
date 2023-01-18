5037b3da2b7ae80c6369fcdc092c184fc4e17d6f
rodrigo-bernardo <rodrigo.bernardo@ua.pt>
2023-01-18 19:18:55 +0000

    1231

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-#from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text
+import pymysql
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 import time
-import mysql.connector
+#import mysql.connector
 
 # database connection information
 host = "sql7.freesqldatabase.com"
@@ -11,10 +12,10 @@ user = "sql7589569"
 password = "NTLDAUBVuH"
 database = "sql7589569"
 
-conn = mysql.connector.connect(host=host, user=user, password=password, database=database)
-c = conn.cursor()
-#conn = create_engine("mysql+pymysql://sql7589569:NTLDAUBVuH@sql7.freesqldatabase.com/sql7589569?charset=utf8mb4")
-
+#conn = mysql.connector.connect(host=host, user=user, password=password, database=database)
+#c = conn.cursor()
+conn = create_engine("mysql+pymysql://sql7589569:NTLDAUBVuH@sql7.freesqldatabase.com/sql7589569?charset=utf8mb4")
+c = conn.connect()
 
 st.set_page_config(layout="wide")
 
@@ -23,21 +24,21 @@ st.write("My First App")
 placeholder = st.empty()
 
 if st.button("RESET"):
-    query = 'DELETE FROM molde1;'
-    c.execute(query)
-    query = 'ALTER TABLE molde1 AUTO_INCREMENT = 1;'
-    c.execute(query)
-    #sql = text('DELETE FROM molde1;')
-    #result = conn.execute(sql)
-    #sql = text('ALTER TABLE molde1 AUTO_INCREMENT = 1;')
-    #result = conn.execute(sql)
+    #query = 'DELETE FROM molde1;'
+    #c.execute(query)
+    #query = 'ALTER TABLE molde1 AUTO_INCREMENT = 1;'
+    #c.execute(query)
+    sql = text('DELETE FROM molde1;')
+    result = c.execute(sql)
+    sql = text('ALTER TABLE molde1 AUTO_INCREMENT = 1;')
+    result = c.execute(sql)
     
 
 while True:
-    #SQL_Query = pd.read_sql('SELECT DATAHORA,T1,P1 FROM molde1', conn)
-    #df = pd.DataFrame(SQL_Query, columns=['DATAHORA','T1','P1'])
-    query = "Select * from molde1;"
-    df = pd.read_sql(query,conn)
+    SQL_Query = pd.read_sql('SELECT DATAHORA,T1,P1 FROM molde1', c)
+    df = pd.DataFrame(SQL_Query, columns=['DATAHORA','T1','P1'])
+    #query = "Select * from molde1;"
+    #df = pd.read_sql(query,conn)
     data = df[["T1", "P1"]]
     fig = px.line(df, x="DATAHORA", y=data.columns,
         labels={
