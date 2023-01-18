864af736a50d5c89c8b08f3fbbcf910dc2070928
rodrigo-bernardo <rodrigo.bernardo@ua.pt>
2023-01-18 19:02:08 +0000

    111

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,20 @@
-from sqlalchemy import create_engine, text
+#from sqlalchemy import create_engine, text
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 import time
+import mysql.connector
 
+# database connection information
+host = "sql7.freesqldatabase.com"
+user = "sql7589569"
+password = "NTLDAUBVuH"
+database = "sql7589569"
 
-conn = create_engine("mysql+pymysql://sql7589569:NTLDAUBVuH@sql7.freesqldatabase.com/sql7589569?charset=utf8mb4")
+conn = mysql.connector.connect(host=host, user=user, password=password, database=database)
+c = conn.cursor()
+#conn = create_engine("mysql+pymysql://sql7589569:NTLDAUBVuH@sql7.freesqldatabase.com/sql7589569?charset=utf8mb4")
+
 
 st.set_page_config(layout="wide")
 
@@ -14,24 +23,29 @@ st.write("My First App")
 placeholder = st.empty()
 
 if st.button("RESET"):
-    sql = text('DELETE FROM molde1;')
-    result = conn.execute(sql)
-    sql = text('ALTER TABLE molde1 AUTO_INCREMENT = 1;')
-    result = conn.execute(sql)
+    query = 'DELETE FROM molde1;'
+    c.execute(query)
+    query = 'ALTER TABLE molde1 AUTO_INCREMENT = 1;'
+    c.execute(query)
+    #sql = text('DELETE FROM molde1;')
+    #result = conn.execute(sql)
+    #sql = text('ALTER TABLE molde1 AUTO_INCREMENT = 1;')
+    #result = conn.execute(sql)
     
 
 while True:
-    SQL_Query = pd.read_sql('SELECT DATAHORA,T1,P1 FROM molde1', conn)
-    df = pd.DataFrame(SQL_Query, columns=['DATAHORA','T1','P1'])
+    #SQL_Query = pd.read_sql('SELECT DATAHORA,T1,P1 FROM molde1', conn)
+    #df = pd.DataFrame(SQL_Query, columns=['DATAHORA','T1','P1'])
+    query = "Select * from molde1;"
+    df = pd.read_sql(query,conn)
     data = df[["T1", "P1"]]
-
     fig = px.line(df, x="DATAHORA", y=data.columns,
-    labels={
-        "DATAHORA": "Data e Hora",
-        "value" : "Valor",
-        "variable" : "Legenda"
-    },
-    title='P1 and T1 sensor values')
+        labels={
+            "DATAHORA": "Data e Hora",
+            "value" : "Valor",
+            "variable" : "Legenda"
+        },
+        title='P1 and T1 sensor values')
 
     fig.update_xaxes(showgrid=True, ticks="inside")
     fig.update_layout({"uirevision": "foo"}, overwrite=True)
