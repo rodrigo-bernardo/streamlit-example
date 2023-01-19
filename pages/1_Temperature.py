101e54917ff004a07d25324f267c6709ae045e3e
rodrigo-bernardo <rodrigo.bernardo@ua.pt>
2023-01-19 22:32:11 +0000

    123

--- pages/1_Temperature.py
+++ pages/1_Temperature.py
@@ -66,7 +66,7 @@ while True:
 
     fig1.update_xaxes(showgrid=True, ticks="inside")
     fig1.update_layout({"uirevision": "foo"}, overwrite=True)
-    print(last_minute)
+ 
     fig2 = px.line(last_minute, x="DATAHORA", y=last_minute_data.columns,
         labels={
             "DATAHORA": "Data e Hora",
