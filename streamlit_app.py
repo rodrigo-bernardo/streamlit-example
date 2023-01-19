b49c074983d54bd8d6dc4aaed9b77cc5339abdad
rodrigo-bernardo <rodrigo.bernardo@ua.pt>
2023-01-19 15:19:46 +0000

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,27 @@ c = conn.connect()
 
 st.set_page_config(layout="wide")
 
+st.markdown("""
+<style>
+div[data-testid="metric-container"] {
+   background-color: rgba(28, 131, 225, 0.1);
+   border: 1px solid rgba(28, 131, 225, 0.1);
+   padding: 5% 5% 5% 10%;
+   border-radius: 5px;
+   color: rgb(30, 103, 119);
+   overflow-wrap: break-word;
+}
+
+/* breakline for metric text         */
+div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {
+   overflow-wrap: break-word;
+   white-space: break-spaces;
+   color: red;
+}
+</style>
+"""
+, unsafe_allow_html=True)
+
 st.write('Projeto "DDS"')
 
 col1, col2 = st.columns(2)
