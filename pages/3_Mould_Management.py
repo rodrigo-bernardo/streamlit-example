ceace29a8e9d6ca7c0227a61a48ea54772384739
rodrigo-bernardo <rodrigo.bernardo@ua.pt>
2023-01-19 23:01:51 +0000

    123

--- pages/3_Mould_Management.py
+++ pages/3_Mould_Management.py
@@ -1,5 +1,21 @@
 import streamlit as st
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 
 st.set_page_config(
     page_title="Mould Management",
     layout="wide")
+
+
+# Connect to the database
+conn = create_engine("mysql+pymysql://sql7589569:NTLDAUBVuH@sql7.freesqldatabase.com/sql7589569?charset=utf8mb4")
+
+# Create a metadata object
+metadata = MetaData()
+
+# Reflect the tables
+metadata.reflect(bind=conn)
+
+# Print the table names
+for table in metadata.tables:
+    print(table)
